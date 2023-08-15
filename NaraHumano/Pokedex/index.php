57b9a0f6489f7ef44495784413aeb20cebdf660d
narahumano1989 <humano.naraesther@gmail.com>
2023-08-15 17:07:22 -0300

    se sube trabajos que faltaban

---
+++ NaraHumano/Pokedex/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+</head>
+<body class="bg-black">
+<header >
+    <img src="./img/logo-pokedex.png">
+    <nav>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+</header>
+<section>
+    <article>
+        <img>
+        <aside>
+            <h2></h2>
+            <img>
+            <p></p>
+        </aside>
+    </article>
+</section>
+</body>
+</html>
